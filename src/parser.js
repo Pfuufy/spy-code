51370c7a02c595a59714b16a16c0f7c0b426ba9c
Gabriel Bissey <gabrielbissey@gmail.com>
2019-09-28 23:17:00 -0600

    FUNCTION: handleNode()

--- src/parser.js
+++ src/parser.js
@@ -1,14 +1,16 @@
+function handleNode(node) {
+    return 1;
+}
+
 function convertToIIFEFunction(abstractSyntaxTree) {
     const ast = abstractSyntaxTree;
     const body = ast.program.body[0].body.body;
 
-    body.forEach(node => {
-        console.log(node);
-
-        try {
-            console.log(node.consequent.argument.name)
-        } catch {}
+    body.forEach((node, i, arr) => {
+        arr[i] = handleNode(node);
     });
+
+    console.log(body);
 }
 
 export { convertToIIFEFunction }
